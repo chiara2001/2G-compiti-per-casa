f15d5600854eff3e42e9cb208bede3a187a8a016
chiara2001 <mamacachiara01@icloud.com>
2017-04-21 22:44:34 +0200

    Add files via upload

---
+++ char.cpp.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main
+{
+    int max=127;
+    int min=-128;
+    // numero massimo è 127;
+    // numero minimo è -128;
+    cout << "Il numero massimo" << max << endl;
+    cout << "Il numero minimo" << min << endl;
+    cout << "Il numero massimo -1" << min-1 << endl;
+    cout << "Il numero massimo +1" << max+1 << endl;
+    return 0;
+}
